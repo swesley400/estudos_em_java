95263685cbc7a0121bf929f37102d37f6a435c44
Wesley Vieira Santos <79053520+swesley400@users.noreply.github.com>
2023-04-09 20:02:42 -0300

    Tipos Primitivos e Variaveis

--- src/TiposPremitivos.java
+++ src/TiposPremitivos.java
@@ -1,11 +1,14 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class TiposPremitivos {
+
     public static void main(String[] args) {
         int year = 2000;
         boolean havePlan = true;
         double note = 8.1;
         String nameOfFilme = "Star Wars";
+
+        System.out.println( year + "\n" + havePlan + "\n" + note + "\n" + nameOfFilme);
     }
 
 }
