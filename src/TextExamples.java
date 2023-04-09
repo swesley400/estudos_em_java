9d095397e175b0d0bd8b5f10242a30abe915c6c4
Wesley Vieira Santos <79053520+swesley400@users.noreply.github.com>
2023-04-09 20:20:49 -0300

    Text Examples + Interterpolações

--- src/TextExamples.java
+++ src/TextExamples.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class TextExamples {
+public class TextExamples {
+    public static void main(String[] args) {
+        String  nome = "Wesley";
+        String mensagem = """
+                    Olá, %s! 
+                    Boas vindas ao curso.
+                    Teremos %d aulas para te mostrar o que é preciso 
+                """ .formatted(nome, 4);
+        System.out.println(mensagem);
+
+    }
+
 }
