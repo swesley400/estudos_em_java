f2899f978104fb991d4cf6d2c6ae1cd9e56854aa
Wesley Vieira Santos <79053520+swesley400@users.noreply.github.com>
2023-04-09 19:56:34 -0300

    Tipos Primitivos e Variaveis

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int year = 2000;
+        boolean havePlan = true;
+        double note = 8.1;
+        String nameOfFilme = "Star Wars";
+    }
+
+}
