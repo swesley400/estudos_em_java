df7fb1074d074792f9874e8c287a79477431f21c
Wesley Vieira Santos <79053520+swesley400@users.noreply.github.com>
2023-04-09 20:34:23 -0300

    Numbers and casts

---
+++ src/Numbers.java
@@ -0,0 +1,12 @@
+public class Numbers {
+
+    /*O que é cast? Mudar o tipo */
+    public static void main(String [] args) {
+        double somas = 8.1 + 8.2 + 4.3 ;
+
+        /*Exemplo de cast*/
+        int media = (int) (somas / 3 / 2);
+
+        System.out.println(media  + "A");
+    }
+}
