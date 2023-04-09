f1881ecd5a43248a5de43d70424e1fc56066c608
Wesley Vieira Santos <79053520+swesley400@users.noreply.github.com>
2023-04-09 20:45:18 -0300

    Exercicio 1: casts

---
+++ src/exercicios/Exercicio1.java
@@ -0,0 +1,17 @@
+package exercicios;
+
+public class Exercicio1 {
+
+    public static void main(String [] args){
+        int temperatura = 18;
+        int formulas = (int)(temperatura * 1.8 + 32);
+
+        int _temperatura = formulas;
+
+
+        System.out.println(_temperatura);
+
+    }
+
+
+}
